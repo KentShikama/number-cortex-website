6145451ddb83ebda6afb41851fad4e1ff3cfc93a
theworldbright <sushionigiris@gmail.com>
2014-06-06 14:02:11 +0900

    Remove site title and description from the top of page

--- wp-content/themes/number-quarto/header.php
+++ wp-content/themes/number-quarto/header.php
@@ -4,7 +4,7 @@
  *
  * Displays all of the <head> section and everything up till <div id="content">
  *
- * @package number quarto
+ * @package Number Quarto
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -23,11 +23,6 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'number-quarto' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'number-quarto' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
