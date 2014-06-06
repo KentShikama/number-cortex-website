2d81a050ce1589e4a5ef24bb7fae5a8971ac1641
theworldbright <sushionigiris@gmail.com>
2014-06-06 16:35:42 +0900

    Show under construction text for all pages

--- wp-content/themes/number-quarto/page.php
+++ wp-content/themes/number-quarto/page.php
@@ -14,22 +14,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
+                    <h1 style="font-family: gills sans; font-size: 3em; padding: 10px; padding-bottom: 30px;">Under Construction</h1>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
