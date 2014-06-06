5d1deca591210bf30b804365ce4117d8acf98598
theworldbright <sushionigiris@gmail.com>
2014-06-06 15:45:00 +0900

    Set up FlexNav

--- wp-content/themes/number-quarto/footer.php
+++ wp-content/themes/number-quarto/footer.php
@@ -8,16 +8,27 @@
  */
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-            Copyright © 2014 Kent Shikama. All rights reserved. <a href="<?php echo get_permalink(26); ?>">Terms of Use</a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        Copyright © 2014 Kent Shikama. All rights reserved. <a href="<?php echo get_permalink(26); ?>">Terms of Use</a>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.flexnav.min.js"></script>
+<script>
+    jQuery(document).ready(function($) {
+        $(".flexnav").flexNav({
+            'animationSpeed': 250, // default for drop down animation speed
+            'transitionOpacity': true, // default for opacity animation
+            'calcItemWidths': false, // dynamically calcs top level nav item widths
+            'hover': true            // would you like hover support?      
+        });
+    });
+</script>
 </body>
 </html>
--- wp-content/themes/number-quarto/header.php
+++ wp-content/themes/number-quarto/header.php
@@ -8,25 +8,32 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php wp_title('|', true, 'right'); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <link href="<?php bloginfo('template_url'); ?>/css/flexnav.css" rel="stylesheet" type="text/css" />
 
-<?php wp_head(); ?>
-</head>
+        <?php wp_head(); ?>
+    </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'number-quarto' ); ?></a>
+    <body <?php body_class(); ?>>
+        <div id="page" class="hfeed site">
+            <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', 'number-quarto'); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'number-quarto' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            <header id="masthead" class="site-header" role="banner">
+                <div class="menu-button">Menu</div>
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'primary',
+                        'menu_class' => 'flexnav', //Adding the class for FlexNav
+                        'items_wrap' => '<ul data-breakpoint="600" id="%1$s" class="%2$s">%3$s</ul>', // Adding data-breakpoint for FlexNav
+                    ));
+                    ?>
+                </nav><!-- #site-navigation -->
+            </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+            <div id="content" class="site-content">
