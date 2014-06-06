713e9619d0b8004d13a5519f5ec335e104ca8638
theworldbright <sushionigiris@gmail.com>
2014-06-06 15:43:38 +0900

    Remove default mobile navigation set up

--- wp-content/themes/number-quarto/functions.php
+++ wp-content/themes/number-quarto/functions.php
@@ -94,8 +94,6 @@ add_action( 'widgets_init', 'number_quarto_widgets_init' );
 function number_quarto_scripts() {
 	wp_enqueue_style( 'number-quarto-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'number-quarto-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'number-quarto-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
