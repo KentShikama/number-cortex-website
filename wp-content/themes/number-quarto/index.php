f712a84cf4db1eeb9b4b0e88151ce916dada591e
theworldbright <sushionigiris@gmail.com>
2014-06-08 19:33:09 +0900

    Fix image linking method

--- wp-content/themes/number-quarto/index.php
+++ wp-content/themes/number-quarto/index.php
@@ -15,9 +15,9 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
                     <div id="numberquartobanner">
-                        <div id="number_quarto_title"><img src="images/number_quarto_title.png" alt="number quarto title" /></div>
-                        <div id="coming_soon"><img src="images/coming_soon.png" alt="coming soon" /></div>
-                        <div id="number_quarto_app_phone"><img src="images/number_quarto_on_iPhone_and_iPod_touch.png" alt="number quarto preview on iPhone and iPod touch" /></div>
+                        <div id="number_quarto_title"><img src="/images/number_quarto_title.png" alt="number quarto title" /></div>
+                        <div id="coming_soon"><img src="/images/coming_soon.png" alt="coming soon" /></div>
+                        <div id="number_quarto_app_phone"><img src="/images/number_quarto_on_iPhone_and_iPod_touch.png" alt="number quarto preview on iPhone and iPod touch" /></div>
                     </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
