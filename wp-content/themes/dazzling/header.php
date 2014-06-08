7b942e1dad3bdb7993c21f0be7f3b8deaa301d50
theworldbright <sushionigiris@gmail.com>
2014-06-08 23:11:00 +0900

    Add number quarto logo and header

--- wp-content/themes/dazzling/header.php
+++ wp-content/themes/dazzling/header.php
@@ -8,56 +8,57 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php wp_title('|', true, 'right'); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-<!-- favicon -->
+        <!-- favicon -->
 
-<?php if ( of_get_option( 'custom_favicon' ) ) { ?>
-<link rel="icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" />
-<?php } ?>
+        <?php if (of_get_option('custom_favicon')) { ?>
+            <link rel="icon" href="<?php echo of_get_option('custom_favicon'); ?>" />
+        <?php } ?>
 
-<!--[if IE]><?php if ( of_get_option( 'custom_favicon' ) ) { ?><link rel="shortcut icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" /><?php } ?><![endif]-->
+<!--[if IE]><?php if (of_get_option('custom_favicon')) { ?><link rel="shortcut icon" href="<?php echo of_get_option('custom_favicon'); ?>" /><?php } ?><![endif]-->
 
-<?php wp_head(); ?>
+        <?php wp_head(); ?>
 
-</head>
+    </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+    <body <?php body_class(); ?>>
+        <div id="page" class="hfeed site">
 
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container">
-		        <div class="navbar-header">
-		            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-		                <span class="sr-only">Toggle navigation</span>
-		                <span class="icon-bar"></span>
-		                <span class="icon-bar"></span>
-		                <span class="icon-bar"></span>
-		            </button>
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-				<?php if( get_header_image() != '' ) : ?>
+                        <?php if (get_header_image() != '') : ?>
 
-					<div id="logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>"  height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
-					</div><!-- end of #logo -->
+                            <div id="logo">
+                                <img src="<?php header_image(); ?>"  height="58" width="58" alt="<?php bloginfo('name'); ?>"/>
+                                <div id="logo_text"><a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">Number Quarto</a></div>
+                            </div><!-- end of #logo -->
 
-				<?php endif; // header image was removed ?>
+                        <?php endif; // header image was removed ?>
 
-				<?php if( !get_header_image() ) : ?>
+                        <?php if (!get_header_image()) : ?>
 
-					<div id="logo">
-						<span class="site-name"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-					</div><!-- end of #logo -->
+                            <div id="logo">
+                                <span class="site-name"><a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a></span>
+                            </div><!-- end of #logo -->
 
-				<?php endif; // header image was removed (again) ?>
+                        <?php endif; // header image was removed (again) ?>
 
-		        </div>
-				<?php dazzling_header_menu(); ?>
-		    </div>
-		</nav><!-- .site-navigation -->
+                    </div>
+                    <?php dazzling_header_menu(); ?>
+                </div>
+            </nav><!-- .site-navigation -->
