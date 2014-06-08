189c639197815c8ab18ff9da59847bf62d7777ef
theworldbright <sushionigiris@gmail.com>
2014-06-08 19:50:00 +0900

    Wrap number quarto banner so that contents are always centered
    Note that there needs to be some adjustments for mobile devices
    that are less than 1024px wide.

--- wp-content/themes/number-quarto/index.php
+++ wp-content/themes/number-quarto/index.php
@@ -10,15 +10,17 @@
  *
  * @package Number Quarto
  */
-
-get_header(); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-                    <div id="numberquartobanner">
-                        <div id="number_quarto_title"><img src="/images/number_quarto_title.png" alt="number quarto title" /></div>
-                        <div id="coming_soon"><img src="/images/coming_soon.png" alt="coming soon" /></div>
-                        <div id="number_quarto_app_phone"><img src="/images/number_quarto_on_iPhone_and_iPod_touch.png" alt="number quarto preview on iPhone and iPod touch" /></div>
-                    </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+get_header();
+?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div id="numberquartobanner">
+            <div id="numberquartobannercontents">
+                <div id="number_quarto_title"><img src="/images/number_quarto_title.png" alt="number quarto title" /></div>
+                <div id="coming_soon"><img src="/images/coming_soon.png" alt="coming soon" /></div>
+                <div id="number_quarto_app_phone"><img src="/images/number_quarto_on_iPhone_and_iPod_touch.png" alt="number quarto preview on iPhone and iPod touch" /></div>
+            </div>
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
 <?php get_footer(); ?>
