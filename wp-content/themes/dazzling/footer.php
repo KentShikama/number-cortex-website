0d4046f63566d9b209c4f0b3b463b5079486cc03
theworldbright <sushionigiris@gmail.com>
2015-01-02 20:30:35 +0900

    Update copyright notice

--- wp-content/themes/dazzling/footer.php
+++ wp-content/themes/dazzling/footer.php
@@ -26,7 +26,7 @@
         <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51ee1b910e0fe34b"></script>
         <!-- AddThis Button END -->
         <div class="site-info">
-            Copyright © 2014 Akane Corporation. All rights reserved. <a href="/terms-use/">Terms of Use</a>
+            Copyright © 2015 Kent Shikama. All rights reserved.
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
 </div>
